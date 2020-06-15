21b4d99e90a9b620b5fdb53c9ab1b0c4f0c828e2
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-06-15 12:10:42 +0200

    Added working sensor reading

--- Boards/sensor_boards/main.c
+++ Boards/sensor_boards/main.c
@@ -2,47 +2,93 @@
 SAUL TEST
 */
 #include <stdio.h>
-#include "saul.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "shell.h"
 #include "thread.h"
 #include "xtimer.h"
+#include "lps331ap.h" //sensors driver 
+#include "lps331ap_params.h" //sensors driver 
+#include "read_sensors.h"
+
 
 char stack[THREAD_STACKSIZE_MAIN];
 
 void *thread_handler(void *arg)
 {
     (void) arg;
-    puts("I'm in \"thread\" now. Try to read sensors using SAUL");
-    puts("Initializing sensors");
-    static saul_driver_t dev;
-    printf("%d ",saul_read_t.read);
-    /*saul_init_devs();
-    phydat_t readings;
-    if( saul_read_t(SAUL_SENSE_ID_TEMP,&readings) == ENOTSUP){
-        puts("Not Supported");
-        exit(1);
-    }else{
-        printf("%d %u %d", readings.val,readings.unit,readings.scale);
-    }*/
-    xtimer_sleep(1);
+
+    while (true)
+    {
+        puts("I'm in \"thread\" now.");
+        puts("I will read sensors periodically");
+        xtimer_sleep(5);
+    }
+    
     return NULL;
 }
 
-static int saul_test_(int argc, char **argv){
+int read_sensors(int *pres, int *temp)
+{
+   
+    static lps331ap_t dev;
+    
+    if (lps331ap_init(&dev, &lps331ap_params[0]) != 0) {
+        puts("Initialization failed");
+        return READ_FAIL;
+    }
 
+    lps331ap_enable(&dev);
+    xtimer_sleep(1); /* wait a bit for the measurements to complete */
+
+    *temp = lps331ap_read_temp(&dev); //temp and pres are already pointers
+    *pres = lps331ap_read_pres(&dev);
+    lps331ap_disable(&dev); //do not waste energy, disable sensors after measurements
+
+    int temp_abs = *temp / 100;
+    *temp -= temp_abs * 100;
+
+    printf("Pressure value: %ihPa - Temperature: %2i.%02i°C\n",
+           *pres, temp_abs, *temp);
+           
+    return READ_OK;
+}
+
+static int cmd_test_sensors(int argc, char **argv)
+{
     (void)argc;
-    (void)argv;
-    puts("Initializing sensors");
-    static saul_driver_t dev;
-   // void const devvv;
-    phydat_t res;
-    dev.read(&dev,&res);
-    printf("%d",res.val[0]);
+    (void)argv; 
+    int temp;
+    int pres;   
+    if( read_sensors(&pres, &temp) == READ_FAIL){ //error reading
+            puts("Unable to read sensors at this round. Not sending anything.\n");
+            
+    }else{ //read sensor ok then print data
+        //Get the current time and print it into the string date_time
+        char date_time[30];
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        sprintf(date_time,"%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+        //define the message as a string and print values in the message string
+        char message[200];
+        sprintf(message, "{\"station_id\":\"%s\",\"timestamp\":\"%s\",\"temperature\":%d,\"pres\":%d}",
+                 "stat_1",date_time,temp,pres);
+        
+        //Print  data.
+        
+        puts("--------------------------------\n\n");
+        printf("Values Read:\n%s\n",message);
+        puts("\n\n--------------------------------\n");
+        
+        }
     return 0;
 }
 
+
 static const shell_command_t shell_commands[] = {
-        {"prova", "SAUL",saul_test_},
+        {"test_sensor", "Read sensors without sending their values and print values in the console",cmd_test_sensors},
         { NULL, NULL, NULL }
 };
 
---
+++ Boards/sensor_boards/read_sensors.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE_SENSORS
+#define HEADER_FILE_SENSORS
+
+int read_sensors(int *pres, int *temp);
+int READ_OK = 0;
+int READ_FAIL = 1;
+
+#endif
