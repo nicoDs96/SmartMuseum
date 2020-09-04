95ee963b6a1b23829c86cfac83a3a42afac5e874
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-09-04 17:53:35 +0200

    forward from board to ttn to db working

--- MuseumAPI/api.js
+++ MuseumAPI/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var ttn = require('ttn');
+const { DataClient } = require('ttn/dist/data');
 var awsIot = require('aws-iot-device-sdk');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
@@ -8,8 +9,8 @@ const assert = require('assert');
 
 //TTN params  https://www.thethingsnetwork.org/docs/applications/nodejs/quick-start.html for doc
 //https://www.thethingsnetwork.org/docs/applications/nodejs/api.html
-var appID = "smart_museum";
-var accessKey = "ttn-account-v2.UwQYpQqxdt1Lz5jaNZyk94qLMy0hLZb4XzCDvRqjYD4";
+var appID = "env_stat";
+var accessKey = "ttn-account-v2.-YKpgPfGoBOeCIURbXDcCA_0nGg_DMwFKveFfiUx2bs";
 //AWS params
 var device = awsIot.device({
     keyPath: '/home/nicods/Scrivania/SmartMuseum/MuseumAPI/AWSCerts/station.private.key',
@@ -154,3 +155,98 @@ app.use(function(req, res, next) {
 app.listen(3000, function () {
     console.log('Museum Middleware listening on port 3000!');
 });
+
+
+/*
+
+CODE NOT BELONGING TO THE API i.e. ttn/mqtt routines
+
+*/
+
+ttn.data(appID, accessKey)
+  .then(function (client) {
+    console.log("Created client");
+    //console.log(client);  
+    client.on("uplink", function (devID, payload) {
+      console.log("Received uplink from ", devID)
+      //Simulation payload: 31 3A 31 2C 32 3A 34 34 2E 37 30 2C 33 3A 34 34 2E 38 34 2C 34 3A 34 34 2E 38 34 2C 35 3A 2C 36 3A 31 33 30 33 2C 37 3A 31 33 30 33
+      let msg = msg2json(payload);
+      msg2db(msg);
+    });
+    console.log("registered uplink callback");
+  })
+  .catch(function (error) {
+    console.error("Error", error)
+    process.exit(1)
+  })
+
+
+//transform lora space saving messages into json format
+var msg2json =  (payload) =>{
+
+  let data = payload.payload_raw.toString();
+  let new_msg={};
+
+  data = data.split(',');
+  data.forEach(el=>{
+    e = el.split(":");
+    e[1]===""?e[1]="0":e[1]=e[1];
+
+    switch (e[0]) {
+      case "1":
+        new_msg['room']=e[1]
+        break;
+      case "2":
+        new_msg['Tem_avg']=e[1]
+        break;
+      case "3":
+        new_msg['Tem_min']=e[1]
+        break;
+      case "4":
+        new_msg['Tem_max']=e[1]
+        break;
+      case "5":
+        new_msg['Hum_avg']=e[1]
+        break;
+      case "6":
+        new_msg['Hum_min']=e[1]
+        break;
+      case "7":
+        new_msg['Hum_max']=e[1]
+        break;
+      default:
+        console.error(`Error, code:${e[0]}\tvalue:${e[1]}` );
+    }
+  });
+  new_msg['datetime']=payload.metadata.time;
+  new_msg['realtime']=false;
+  console.log(new_msg);
+  return new_msg;
+}
+
+//insert one room data into the db
+var msg2db = async (room)=>{
+  try{
+    //let urii ="mongodb://localhost:27017";
+    //let urii= "mongodb+srv://dbUser:dbUser@cluster0-sa1g1.mongodb.net/SmartMuseum?retryWrites=true&w=majority"
+
+
+    // Use connect method to connect to the Server
+    await client.connect( async err => {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+      const db = client.db("SmartMuseum");
+      //collection.insertOne(room);
+      await db.collection("Rooms").insertOne(room, (err, r) => {
+        assert.equal(null, err);
+        assert.equal(1, r.insertedCount);
+      });
+        
+    });
+
+    client.close();
+  } 
+  catch(e){
+    console.error(e);
+  }
+}
--- MuseumAPI/test/ttn_test.js
+++ MuseumAPI/test/ttn_test.js
@@ -13,44 +13,14 @@ ttn.data(appID, accessKey)
     //console.log(client);  
     client.on("uplink", function (devID, payload) {
       console.log("Received uplink from ", devID)
-      //console.log(payload)    
-      //Simulation payload: 31 3A 22 31 22 2C 32 3A 34 34 2E 37 30 2C 33 3A 34 34 2E 38 34 2C 34 3A 34 34 2E 38 34 2C 35 3A 2C 36 3A 31 33 30 33 2C 37 3A 31 33 30 33
-      console.log(payload.payload_raw.toString());
-      let data = payload.payload_raw.toString();
-      let new_msg={};
-      data = data.split(',');
-      data.forEach(el=>{
-        e = el.split(":");
-        e[1]===""?e[1]="0":e[1]=e[1];
-
-        switch (e[0]) {
-          case "1":
-            new_msg['room']=e[1]
-            break;
-          case "2":
-            new_msg['Tem_avg']=e[1]
-            break;
-          case "3":
-            new_msg['Tem_min']=e[1]
-            break;
-          case "4":
-            new_msg['Tem_max']=e[1]
-            break;
-          case "5":
-            new_msg['Hum_avg']=e[1]
-            break;
-          case "6":
-            new_msg['Hum_min']=e[1]
-            break;
-          case "7":
-            new_msg['Hum_max']=e[1]
-            break;
-          default:
-            console.error(`Error, code:${e[0]}\tvalue:${e[1]}` );
-        }
-      });
-
+      //Simulation payload: 31 3A 31 2C 32 3A 34 34 2E 37 30 2C 33 3A 34 34 2E 38 34 2C 34 3A 34 34 2E 38 34 2C 35 3A 2C 36 3A 31 33 30 33 2C 37 3A 31 33 30 33
+      let msg = msg2json(payload);
       
+      
+      msg2db(msg);
+
+      console.log("trying uplink to: "+ devID);
+      client.send(devID,new Buffer(JSON.stringify({realtime:true}),"hex"),1,false,"replace");
 
     });
     console.log("registered uplink callback");
@@ -59,3 +29,73 @@ ttn.data(appID, accessKey)
     console.error("Error", error)
     process.exit(1)
   })
+
+
+
+var msg2json =  (payload) =>{
+
+  let data = payload.payload_raw.toString();
+  let new_msg={};
+
+  data = data.split(',');
+  data.forEach(el=>{
+    e = el.split(":");
+    e[1]===""?e[1]="0":e[1]=e[1];
+
+    switch (e[0]) {
+      case "1":
+        new_msg['room']=e[1]
+        break;
+      case "2":
+        new_msg['Tem_avg']=e[1]
+        break;
+      case "3":
+        new_msg['Tem_min']=e[1]
+        break;
+      case "4":
+        new_msg['Tem_max']=e[1]
+        break;
+      case "5":
+        new_msg['Hum_avg']=e[1]
+        break;
+      case "6":
+        new_msg['Hum_min']=e[1]
+        break;
+      case "7":
+        new_msg['Hum_max']=e[1]
+        break;
+      default:
+        console.error(`Error, code:${e[0]}\tvalue:${e[1]}` );
+    }
+  });
+  new_msg['datetime']=payload.metadata.time;
+  new_msg['realtime']=false;
+  console.log(new_msg);
+  return new_msg;
+}
+
+var msg2db = async (msg)=>{
+  try{
+    //let urii ="mongodb://localhost:27017";
+    //let urii= "mongodb+srv://dbUser:dbUser@cluster0-sa1g1.mongodb.net/SmartMuseum?retryWrites=true&w=majority"
+
+
+    // Use connect method to connect to the Server
+    await client.connect( async err => {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+      const db = client.db("SmartMuseum");
+      //collection.insertOne(room);
+      await db.collection("Rooms").insertOne(room, (err, r) => {
+        assert.equal(null, err);
+        assert.equal(1, r.insertedCount);
+      });
+        
+    });
+
+    client.close();
+  } 
+  catch(e){
+    console.error(e);
+  }
+}
